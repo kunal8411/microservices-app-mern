dc04ea86998e0edd5ac2b325b18f4c41737178b9
Kunal Khairnar <kunal.khairnar96@gmail.com>
2024-06-18 17:24:37 +0530

    added event bus - storage so that we have the all events stored inside event bus storage

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
-const axios = require('axios')
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.post('/events', (req,res)=>{
-    const event = req.body;
-    console.log("EVENT IS", event)
-    axios.post('http://localhost:4000/events', event)
-    axios.post('http://localhost:4001/events', event)
-    axios.post('http://localhost:4002/events', event)
-    axios.post('http://localhost:4003/events', event)
 
-    res.send({status:"OK"})
-})
+const events = [];
+app.post("/events", (req, res) => {
+  const event = req.body;
+  events.push(event);
+  console.log("EVENT IS", event);
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
 
-app.listen(4005,(req,res)=>{
-    console.log("Server listening on 4005")
-})
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, (req, res) => {
+  console.log("Server listening on 4005");
+});
--- query/index.js
+++ query/index.js
@@ -9,12 +9,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const posts = {};
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-  console.log(" type, body type, body type, body", req.body);
+
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -33,9 +29,26 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  console.log(" type, body type, body type, body", req.body);
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
+ try {
   console.log("Posts server listening on 4002");
+  const res = await axios.get('http://localhost:4005/events')
+  for(let event of res.data){
+    console.log("processing event", event.type);
+    handleEvent(event.type, event.data)
+  }
+ } catch (error) {
+  console.log("error is ", error)
+ }
 });
